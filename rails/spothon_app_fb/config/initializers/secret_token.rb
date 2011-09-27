2a29c6f6d5442e7794c2c2eed0cc258830f9a1b7
Takanao Yoshii <tyoshii716@yahoo.co.jp>
2011-09-28 00:29:05 +0900

    fb app mock

---
+++ rails/spothon_app_fb/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpothonAppFb::Application.config.secret_token = '41e44ac7892c833edc37870ca120ab9066ab3bbfb430a6fae8b6a60cb67f3397721002f2d873172e372664c8239a241b3b6f4c5617600dfa68d900a4cc201dd9'
